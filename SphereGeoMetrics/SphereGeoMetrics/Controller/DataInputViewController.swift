df7b675b016654647ea52ffced417fa1ee87d341
san2ride <sanchezjason@me.com>
2017-10-24 13:08:07 -0600

    Sphere - textfield/labels

---
+++ SphereGeoMetrics/SphereGeoMetrics/Controller/DataInputViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DataInputViewController.swift
+//  SphereGeoMetrics
+//
+//  Created by don't touch me on 10/24/17.
+//  Copyright © 2017 trvl, LLC. All rights reserved.
+//
+
+import UIKit
+
+class DataInputViewController: UIViewController {
+    
+    @IBOutlet var firstNameField: UITextField!
+    @IBOutlet var lastNameField: UITextField!
+    @IBOutlet var streetField: UITextField!
+    @IBOutlet var cityField: UITextField!
+    @IBOutlet var stateField: UITextField!
+    @IBOutlet var zipCodeField: UITextField!
+    
+    @IBAction func saveButtonPushed(_ sender: UIButton) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    
+
+}
